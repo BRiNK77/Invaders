4f4e4757b2d71f2ef424543c815428e3b98e00bf
Tristan Zaleski <31313958+BRiNK77@users.noreply.github.com>
2019-12-09 12:45:43 -0600

    Clean up Sesh
    Refined several elements, such as UI placement, functionality, as well as NPC text script. Game can now be played from Start menu to lvl 1 currently.

---
+++ Invaders/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            
+            if (!pauseMenu.activeInHierarchy)
+            {
+                PlayerStats.canShoot = false;
+                PauseGame();
+                Cursor.lockState = CursorLockMode.None;
+                
+            } else if (pauseMenu.activeInHierarchy)
+            {
+                PlayerStats.canShoot = true;
+                ContinueGame();
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
