dd06a6c573bd01848248852dba997b7b8f4a76f8
Tristan Zaleski <31313958+BRiNK77@users.noreply.github.com>
2019-12-09 07:47:32 -0600

    Major additions
    Added several scripts such as character mouse view, shooting script, playerstats, and bullet script along with several prefabs. All were used to further develop the core game, and their were some minor visual updates to the tutorial level.

---
+++ Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int HP = 2;
+    public int DAM = 1;
+    public int range = 5;
+
+    GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int dama)
+    {
+        Debug.Log("damaged. 3");
+        HP -= dama;
+
+        if(HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
